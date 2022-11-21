562e27845ba2c7c9dbe67d7b546a4aacc265e755
Rio Boy <boyrio68@gmail.com>
2022-11-21 16:25:59 +0800

    Install bootstrap, react-bootstrap and delete unuse file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, quod
+        eligendi animi temporibus amet optio similique aspernatur vel hic atque
+        quos, officia cupiditate cum. A voluptate hic nesciunt eos excepturi
+        soluta aspernatur nostrum temporibus, obcaecati ut corrupti ab debitis,
+        alias fuga amet vitae voluptatum molestiae tempora! Nam assumenda et
+        ipsa consectetur porro doloremque odio, corrupti cum est harum quo
+        voluptas. Quae culpa nihil distinctio, facilis expedita atque quos,
+        rerum perspiciatis magnam neque repudiandae quasi? Deleniti ex, amet ab
+        obcaecati placeat expedita iusto voluptatem doloremque eos
+        exercitationem odio repellat repellendus fugiat illo sed molestiae culpa
+        consectetur veritatis architecto non adipisci in.
+      </p>
     </div>
   );
 }
